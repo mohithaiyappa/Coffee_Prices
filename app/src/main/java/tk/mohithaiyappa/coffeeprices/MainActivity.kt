53397ef4b2d6aed0b6dfeed10978e8e4ccce47bc
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-03-15 11:52:02 +0530

    Using object instances from dagger

--- app/src/main/java/tk/mohithaiyappa/coffeeprices/MainActivity.kt
+++ app/src/main/java/tk/mohithaiyappa/coffeeprices/MainActivity.kt
@@ -1,7 +1,7 @@
 package tk.mohithaiyappa.coffeeprices
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
@@ -10,11 +10,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import tk.mohithaiyappa.coffeeprices.adapter.RecyclerViewAdapter
 import tk.mohithaiyappa.coffeeprices.repository.CoffeePricesApi
-import tk.mohithaiyappa.coffeeprices.repository.CoffeePricesData
 import tk.mohithaiyappa.coffeeprices.repository.CoffeePricesDataList
+import javax.inject.Inject
 
 
 class MainActivity : AppCompatActivity() {
+
     private var pricesList : List<CoffeePricesDataList.Data> = emptyList()
     private lateinit var newData : CoffeePricesDataList.Data
     private lateinit var oldData : CoffeePricesDataList.Data
@@ -22,9 +23,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var layoutManager: LinearLayoutManager
     //private var runAgain:Boolean=true
 
+    @Inject
+    lateinit var service: CoffeePricesApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as CoffeeApplication).coffeePriceComponent.inject(this)
+
         //while (runAgain){
             getData()
         //}
@@ -33,6 +40,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     fun setupRecyclerView(){
         layoutManager = LinearLayoutManager(this)
         recycler_view.layoutManager =layoutManager
@@ -40,9 +48,9 @@ class MainActivity : AppCompatActivity() {
         adapter = RecyclerViewAdapter(newData,oldData,this)
         recycler_view.adapter= adapter
     }
+
     fun getData(){
         //Toast.makeText(this,"in get data method",Toast.LENGTH_SHORT).show()
-        val service = CoffeePricesApi.startApi()
         GlobalScope.launch(Dispatchers.IO) {
             val response = service.getAllPrices()
 
@@ -51,7 +59,6 @@ class MainActivity : AppCompatActivity() {
                 oldData= response.body()!!.mData[1]
                 //pricesList=response.body()!!.mData.prices
                 withContext(Dispatchers.Main){
-                    //runAgain=!response.isSuccessful
                     setupRecyclerView()
                 }
             }
--- app/src/main/java/tk/mohithaiyappa/coffeeprices/repository/room/PricesDatabase.kt
+++ app/src/main/java/tk/mohithaiyappa/coffeeprices/repository/room/PricesDatabase.kt
@@ -1,31 +1,11 @@
 package tk.mohithaiyappa.coffeeprices.repository.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
-private const val DATABASE= "price"
-@Database(  entities = arrayOf(
-                ScrappedAtEntity::class,
-                PricesEntity::class),
-            version = 1)
+const val DATABASE= "price"
+
+@Database(entities = [ScrappedAtEntity::class, PricesEntity::class], version = 1)
 abstract class PricesDatabase : RoomDatabase() {
     abstract fun pricesDao():PricesDao
-
-    companion object{
-        @Volatile
-        private var instance :PricesDatabase? = null
-
-        fun getInstance(context:Context):PricesDatabase{
-            return instance?: synchronized(this){
-                instance
-                    ?: buildDatabase(context).also { instance=it }
-            }
-        }
-
-        private fun buildDatabase(context:Context):PricesDatabase{
-            return Room.databaseBuilder(context,PricesDatabase::class.java, DATABASE).build()
-        }
-    }
 }
