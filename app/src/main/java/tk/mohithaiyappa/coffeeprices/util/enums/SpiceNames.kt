770e7d3cd6d32f9398d730110181670a46351dcd
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-04-26 11:32:57 +0530

    created enums for spice names

---
+++ app/src/main/java/tk/mohithaiyappa/coffeeprices/util/enums/SpiceNames.kt
@@ -0,0 +1,9 @@
+package tk.mohithaiyappa.coffeeprices.util.enums
+
+enum class SpiceNames(value:String) {
+    ARABICA_PARCHMENT("Arabica Parchment"),
+    ARABICA_CHERRY("Arabica Cherry"),
+    ROBUSTA_PARCHMENT("Robusta Parchment"),
+    ROBUSTA_CHERRY("Robusta Cherry"),
+    PEPPER("Pepper");
+}
