49348327ed64ab0a0aa70a8219e4d6029b910869
mohithaiyappa <mohithaiyappa229@gmail.com>
2020-12-28 21:45:42 +0530

    OneSignal added

--- app/src/main/java/tk/mohithaiyappa/coffeeprices/CoffeApplication.kt
+++ app/src/main/java/tk/mohithaiyappa/coffeeprices/CoffeApplication.kt
@@ -2,6 +2,7 @@ package tk.mohithaiyappa.coffeeprices
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
+import com.onesignal.OneSignal
 import tk.mohithaiyappa.coffeeprices.di.CoffeePriceComponent
 import tk.mohithaiyappa.coffeeprices.di.DaggerCoffeePriceComponent
 import tk.mohithaiyappa.coffeeprices.di.modules.ContextModule
@@ -9,14 +10,23 @@ import tk.mohithaiyappa.coffeeprices.di.modules.ContextModule
 class CoffeeApplication: Application() {
 
     lateinit var coffeePriceComponent: CoffeePriceComponent
-    var adMobAppID ="ca-app-pub-2357150776599621~8962927399"
+    var adMobAppID = "ca-app-pub-2357150776599621~8962927399"
     var bannerAdID = "ca-app-pub-2357150776599621/9348912538"
     var bannerAdIDTest = "ca-app-pub-3940256099942544/6300978111"
+
+    private val ONESIGNAL_APP_ID = "ed6d45a1-3b1f-47ba-9990-155681e241c5"
 
     override fun onCreate() {
         super.onCreate()
 
         MobileAds.initialize(this) {}
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
 
         coffeePriceComponent = DaggerCoffeePriceComponent
             .builder()
